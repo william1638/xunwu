57c607cf3f8a6e818d5ba96607d93d80986eb0b2
yezewei <yezewei@tourongjia.com>
2018-02-02 15:02:00 +0800

    rt

---
+++ src/main/java/com/will/xunwu/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.will.xunwu.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware{
+
+    private ApplicationContext applicationContext ;
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+
+}
